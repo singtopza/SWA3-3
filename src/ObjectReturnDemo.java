005675a8e17e5568066c00c4fb100ac063a9aed8
singtopza <59428798+singtopza@users.noreply.github.com>
2021-07-19 15:19:50 +0700

    Initial commit

---
+++ src/ObjectReturnDemo.java
@@ -0,0 +1,19 @@
+// Java program to demonstrate returning
+// of objects
+class ObjectReturnDemo
+{
+    int a;
+  
+    ObjectReturnDemo(int i)
+    {
+        a = i;
+    }
+  
+    // This method returns an object
+    ObjectReturnDemo incrByTen()
+    {
+        ObjectReturnDemo temp =
+               new ObjectReturnDemo(a+10);
+        return temp;
+    }
+}
